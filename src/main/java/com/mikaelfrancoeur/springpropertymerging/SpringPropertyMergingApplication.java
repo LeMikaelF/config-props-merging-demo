5fe62cdd5d5f0f8b4382987060af65a045ff7033
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2024-06-26 21:39:04 -0400

    Initial commit

---
+++ src/main/java/com/mikaelfrancoeur/springpropertymerging/SpringPropertyMergingApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.springpropertymerging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertyMergingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPropertyMergingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/springpropertymerging/SpringPropertyMergingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.springpropertymerging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertyMergingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
